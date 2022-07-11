b55dd7afb45c72ab597b747c71c64cd007936cee
Debian <admin@ip-172-31-29-156.ap-southeast-1.compute.internal>
2022-07-11 09:48:29 +0000

    update ipaddr function

--- local-ip-address/go.mod
+++ local-ip-address/go.mod
@@ -1,3 +1,5 @@
 module ipaddr
 
 go 1.17
+
+require github.com/aws/aws-lambda-go v1.32.1 // indirect
--- local-ip-address/main.go
+++ local-ip-address/main.go
@@ -1,24 +1,22 @@
-package main 
+package main
 
 import (
-	"context"
 	"github.com/aws/aws-lambda-go/lambda"
 
 	"log"
 	"net"
-	"strings"
 )
 
 func GetOutboundIP() net.IP {
-    conn, err := net.Dial("udp", "8.8.8.8:80")
-    if err != nil {
-        log.Fatal(err)
-    }
-    defer conn.Close()
+	conn, err := net.Dial("udp", "8.8.8.8:80")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer conn.Close()
 
-    localAddr := conn.LocalAddr().(*net.UDPAddr)
+	localAddr := conn.LocalAddr().(*net.UDPAddr)
 
-    return localAddr.IP
+	return localAddr.IP
 }
 
 func main() {
